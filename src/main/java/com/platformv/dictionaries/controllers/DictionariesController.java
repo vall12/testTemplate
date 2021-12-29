e673aea33a918263992d26441fc68ca8f758c6d6
Андрей Гавриленко <AAGavrilenko@sberbank.ru>
2021-12-29 14:44:12 +0300

    BENDR-379 test speed 1

--- src/main/java/com/platformv/dictionaries/controllers/DictionariesController.java
+++ src/main/java/com/platformv/dictionaries/controllers/DictionariesController.java
@@ -1,7 +1,7 @@
 package com.platformv.dictionaries.controllers;
-
-import com.platformv.dictionaries.controllers.contracts.DictionariesContract;
-import com.platformv.dictionaries.services.UfsService;
+//import com.platformv.dictionaries.controllers.contracts.DictionariesContract;
+//import com.platformv.dictionaries.services.UfsService;
+//
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,44 +24,42 @@ import java.util.stream.Collectors;
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/dictionary")
-public class DictionariesController implements DictionariesContract {
+public class DictionariesController {
 
-    private static final String DICTIONARY_PREFIX = "dictionary/";
-
-    private UfsService dictionaryService;
-
-    @Autowired
-    public DictionariesController(UfsService dictionaryService) {
-        this.dictionaryService = dictionaryService;
-    }
-
-    @PostMapping("/{dictName}/rows")
-    public Row[] getRows(@PathVariable String dictName, @RequestBody QueryRequest query) {
-        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/rows", query, Row[].class);
-    }
-
-    @PostMapping("/{dictName}/row")
-    public Row getRow(@PathVariable String dictName, @RequestBody QueryRequest query) {
-        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/row", query, Row.class);
-    }
-
-    @PostMapping("/{dictName}/count")
-    public Long getCount(@PathVariable String dictName, @RequestBody CountQueryRequest query) {
-        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/count", query, Long.class);
-    }
-
-    @GetMapping("/{dictName}")
-    public Row[] getByCodes(@PathVariable String dictName, @RequestParam List<String> codes) {
-        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromPath(DICTIONARY_PREFIX + dictName);
-        codes.forEach(code -> uriBuilder.queryParam("codes", code));
-        return dictionaryService.get(uriBuilder.toUriString(), Row[].class);
-    }
+//    private static final String DICTIONARY_PREFIX = "dictionary/";
+//
+//    private UfsService dictionaryService;
+//
+//    @Autowired
+//    public DictionariesController(UfsService dictionaryService) {
+//        this.dictionaryService = dictionaryService;
+//    }
+//
+//    @PostMapping("/{dictName}/rows")
+//    public Row[] getRows(@PathVariable String dictName, @RequestBody QueryRequest query) {
+//        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/rows", query, Row[].class);
+//    }
+//
+//    @PostMapping("/{dictName}/row")
+//    public Row getRow(@PathVariable String dictName, @RequestBody QueryRequest query) {
+//        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/row", query, Row.class);
+//    }
+//
+//    @PostMapping("/{dictName}/count")
+//    public Long getCount(@PathVariable String dictName, @RequestBody CountQueryRequest query) {
+//        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/count", query, Long.class);
+//    }
+//
+//    @GetMapping("/{dictName}")
+//    public Row[] getByCodes(@PathVariable String dictName, @RequestParam List<String> codes) {
+//        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromPath(DICTIONARY_PREFIX + dictName);
+//        codes.forEach(code -> uriBuilder.queryParam("codes", code));
+//        return dictionaryService.get(uriBuilder.toUriString(), Row[].class);
+//    }
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello Platform V";
     }
-
-
 
 }
--- src/main/java/com/platformv/dictionaries/controllers/contracts/DictionariesContract.java
+++ src/main/java/com/platformv/dictionaries/controllers/contracts/DictionariesContract.java
@@ -1,54 +1,54 @@
-package com.platformv.dictionaries.controllers.contracts;
-
-import com.platformv.dictionaries.configuration.SwaggerConfig;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.sbrf.ufs.provider.api.dto.dictionary.CountQueryRequest;
-import ru.sbrf.ufs.provider.api.dto.dictionary.QueryRequest;
-import ru.sbrf.ufs.provider.api.dto.dictionary.Row;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.List;
-
-@Api(tags = {SwaggerConfig.DICTIONARIES_TAG})
-@EnableSwagger2
-public interface DictionariesContract {
-
-    String dictionariesNames = "Примеры наименования справочника:\n" +
-            "OKEI\n" +
-            "OKVED\n" +
-            "OKOPF\n";
-
-    String attributes = "Пример атрибутов: \n" +
-            "nationalCode\n" +
-            "code\n" +
-            "nationalSymbol\n" +
-            "internationalCode\n" +
-            "name\n" +
-            "internationalSymbol";
-
-    @ApiOperation("Поиск строк справочника")
-    Row[] getRows(@ApiParam(value = dictionariesNames) @PathVariable String dictName,
-                  @ApiParam(value = attributes) @RequestBody QueryRequest query);
-
-    @ApiOperation("Поиск строки справочника")
-    Row getRow(@ApiParam(value = dictionariesNames) @PathVariable String dictName,
-               @ApiParam(value = attributes) @RequestBody QueryRequest query);
-
-    @ApiOperation("Получение числа строк справочника, удовлетворяющих фильтру")
-    Long getCount(@ApiParam(value = dictionariesNames) @PathVariable String dictName,
-            @ApiParam(value = "Пример: \n" +
-                    "\"filter\": null")
-            @RequestBody CountQueryRequest query);
-
-    @ApiOperation("Получение строк справочника по наименованию и коду")
-    Row[] getByCodes(@ApiParam(value = dictionariesNames) @PathVariable String dictName,
-            @ApiParam(value = "Примеры кода справочника:\n" +
-                            "для OKEI - 003, 004, 005\n" +
-                            "для OKVED - A, A.01, A.01.1\n" +
-                            "для OKOPF - 10000, 11000, 12000\n") @RequestParam List<String> codes);
-}
+//package com.platformv.dictionaries.controllers.contracts;
+//
+//import com.platformv.dictionaries.configuration.SwaggerConfig;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.ApiParam;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import ru.sbrf.ufs.provider.api.dto.dictionary.CountQueryRequest;
+//import ru.sbrf.ufs.provider.api.dto.dictionary.QueryRequest;
+//import ru.sbrf.ufs.provider.api.dto.dictionary.Row;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//import java.util.List;
+//
+//@Api(tags = {SwaggerConfig.DICTIONARIES_TAG})
+//@EnableSwagger2
+//public interface DictionariesContract {
+//
+//    String dictionariesNames = "Примеры наименования справочника:\n" +
+//            "OKEI\n" +
+//            "OKVED\n" +
+//            "OKOPF\n";
+//
+//    String attributes = "Пример атрибутов: \n" +
+//            "nationalCode\n" +
+//            "code\n" +
+//            "nationalSymbol\n" +
+//            "internationalCode\n" +
+//            "name\n" +
+//            "internationalSymbol";
+//
+//    @ApiOperation("Поиск строк справочника")
+//    Row[] getRows(@ApiParam(value = dictionariesNames) @PathVariable String dictName,
+//                  @ApiParam(value = attributes) @RequestBody QueryRequest query);
+//
+//    @ApiOperation("Поиск строки справочника")
+//    Row getRow(@ApiParam(value = dictionariesNames) @PathVariable String dictName,
+//               @ApiParam(value = attributes) @RequestBody QueryRequest query);
+//
+//    @ApiOperation("Получение числа строк справочника, удовлетворяющих фильтру")
+//    Long getCount(@ApiParam(value = dictionariesNames) @PathVariable String dictName,
+//            @ApiParam(value = "Пример: \n" +
+//                    "\"filter\": null")
+//            @RequestBody CountQueryRequest query);
+//
+//    @ApiOperation("Получение строк справочника по наименованию и коду")
+//    Row[] getByCodes(@ApiParam(value = dictionariesNames) @PathVariable String dictName,
+//            @ApiParam(value = "Примеры кода справочника:\n" +
+//                            "для OKEI - 003, 004, 005\n" +
+//                            "для OKVED - A, A.01, A.01.1\n" +
+//                            "для OKOPF - 10000, 11000, 12000\n") @RequestParam List<String> codes);
+//}
