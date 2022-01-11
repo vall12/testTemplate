5753170c2ffda865b486424cb3a4281f54e7008c
Андрей Гавриленко <AAGavrilenko@sberbank.ru>
2022-01-11 14:52:22 +0300

    BENDR-379 with platform service

--- src/main/java/com/platformv/dictionaries/controllers/DictionariesController.java
+++ src/main/java/com/platformv/dictionaries/controllers/DictionariesController.java
@@ -1,19 +1,19 @@
 package com.platformv.dictionaries.controllers;
 
-//import com.platformv.dictionaries.controllers.contracts.DictionariesContract;
-//import com.platformv.dictionaries.services.UfsService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.util.UriComponentsBuilder;
-//import ru.sbrf.ufs.provider.api.dto.dictionary.CountQueryRequest;
-//import ru.sbrf.ufs.provider.api.dto.dictionary.QueryRequest;
-//import ru.sbrf.ufs.provider.api.dto.dictionary.Row;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.stream.Collectors;
+import com.platformv.dictionaries.controllers.contracts.DictionariesContract;
+import com.platformv.dictionaries.services.UfsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.sbrf.ufs.provider.api.dto.dictionary.CountQueryRequest;
+import ru.sbrf.ufs.provider.api.dto.dictionary.QueryRequest;
+import ru.sbrf.ufs.provider.api.dto.dictionary.Row;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,36 +25,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/dictionary")
 public class DictionariesController {
 
-//    private static final String DICTIONARY_PREFIX = "dictionary/";
-//
-//    private UfsService dictionaryService;
-//
-//    @Autowired
-//    public DictionariesController(UfsService dictionaryService) {
-//        this.dictionaryService = dictionaryService;
-//    }
-//
-//    @PostMapping("/{dictName}/rows")
-//    public Row[] getRows(@PathVariable String dictName, @RequestBody QueryRequest query) {
-//        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/rows", query, Row[].class);
-//    }
-//
-//    @PostMapping("/{dictName}/row")
-//    public Row getRow(@PathVariable String dictName, @RequestBody QueryRequest query) {
-//        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/row", query, Row.class);
-//    }
-//
-//    @PostMapping("/{dictName}/count")
-//    public Long getCount(@PathVariable String dictName, @RequestBody CountQueryRequest query) {
-//        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/count", query, Long.class);
-//    }
-//
-//    @GetMapping("/{dictName}")
-//    public Row[] getByCodes(@PathVariable String dictName, @RequestParam List<String> codes) {
-//        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromPath(DICTIONARY_PREFIX + dictName);
-//        codes.forEach(code -> uriBuilder.queryParam("codes", code));
-//        return dictionaryService.get(uriBuilder.toUriString(), Row[].class);
-//    }
+    private static final String DICTIONARY_PREFIX = "dictionary/";
+
+    private UfsService dictionaryService;
+
+    @Autowired
+    public DictionariesController(UfsService dictionaryService) {
+        this.dictionaryService = dictionaryService;
+    }
+
+    @PostMapping("/{dictName}/rows")
+    public Row[] getRows(@PathVariable String dictName, @RequestBody QueryRequest query) {
+        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/rows", query, Row[].class);
+    }
+
+    @PostMapping("/{dictName}/row")
+    public Row getRow(@PathVariable String dictName, @RequestBody QueryRequest query) {
+        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/row", query, Row.class);
+    }
+
+    @PostMapping("/{dictName}/count")
+    public Long getCount(@PathVariable String dictName, @RequestBody CountQueryRequest query) {
+        return dictionaryService.post(DICTIONARY_PREFIX + dictName + "/count", query, Long.class);
+    }
+
+    @GetMapping("/{dictName}")
+    public Row[] getByCodes(@PathVariable String dictName, @RequestParam List<String> codes) {
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromPath(DICTIONARY_PREFIX + dictName);
+        codes.forEach(code -> uriBuilder.queryParam("codes", code));
+        return dictionaryService.get(uriBuilder.toUriString(), Row[].class);
+    }
 
     @GetMapping("/hello")
     public String hello() {
--- src/main/java/com/platformv/dictionaries/services/UfsService.java
+++ src/main/java/com/platformv/dictionaries/services/UfsService.java
@@ -1,82 +1,82 @@
-//package com.platformv.dictionaries.services;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.HttpEntity;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.client.RestTemplate;
-//
-//@Service
-//public class UfsService {
-//
-//    @Value("${ufs.provider.url}")
-//    private String ufsProviderUrl;
-//
-//    public <TResponse> TResponse get(String endpoint, Class<TResponse> responseClass) {
-//        RestTemplate restTemplate = new RestTemplate();
-//        ResponseEntity<TResponse> result = restTemplate.exchange(ufsProviderUrl + "/" + endpoint,
-//                HttpMethod.GET, null, responseClass);
-//        return result.getBody();
-//    }
-//
-//    public <TResponse, TRequest> TResponse post(String endpoint, TRequest request, Class<TResponse> responseClass) {
-//        HttpEntity httpEntity = new HttpEntity(request);
-//        RestTemplate restTemplate = new RestTemplate();
-//        ResponseEntity<TResponse> result = restTemplate.postForEntity(ufsProviderUrl + "/" + endpoint, httpEntity, responseClass);
-//        return result.getBody();
-//    }
-//
-//    public <TRequest> void post(String endpoint, TRequest request) {
-//        HttpEntity httpEntity = new HttpEntity(request);
-//        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.postForEntity(ufsProviderUrl + "/" + endpoint, httpEntity, Void.class);
-//    }
-//
-//    public <TRequest> void put(String endpoint, TRequest request) {
-//        HttpEntity httpEntity = new HttpEntity(request);
-//        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.exchange(ufsProviderUrl + "/" + endpoint, HttpMethod.PUT, httpEntity, Void.class);
-//    }
-//
-//    public void delete(String endpoint) {
-//        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.exchange(ufsProviderUrl + "/" + endpoint, HttpMethod.DELETE, null, Void.class);
-//    }
-//
-//    public <TResponse> TResponse get(String endpoint, HttpHeaders headers, Class<TResponse> responseClass) {
-//        HttpEntity httpEntity = new HttpEntity(headers);
-//        RestTemplate restTemplate = new RestTemplate();
-//        ResponseEntity<TResponse> result = restTemplate.exchange(ufsProviderUrl + "/" + endpoint,
-//                HttpMethod.GET, httpEntity, responseClass);
-//        return result.getBody();
-//    }
-//
-//    public <TResponse, TRequest> TResponse post(String endpoint, TRequest request, HttpHeaders headers, Class<TResponse> responseClass) {
-//        HttpEntity httpEntity = new HttpEntity(request, headers);
-//        RestTemplate restTemplate = new RestTemplate();
-//        ResponseEntity<TResponse> result = restTemplate.postForEntity(ufsProviderUrl + "/" + endpoint, httpEntity, responseClass);
-//        return result.getBody();
-//    }
-//
-//    public <TRequest> void post(String endpoint, TRequest request, HttpHeaders headers) {
-//        HttpEntity httpEntity = new HttpEntity(request, headers);
-//        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.postForEntity(ufsProviderUrl + "/" + endpoint, httpEntity, Void.class);
-//    }
-//
-//    public <TRequest> void put(String endpoint, TRequest request, HttpHeaders headers) {
-//        HttpEntity httpEntity = new HttpEntity(request, headers);
-//        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.exchange(ufsProviderUrl + "/" + endpoint, HttpMethod.PUT, httpEntity, Void.class);
-//    }
-//
-//    public void delete(String endpoint, HttpHeaders headers) {
-//        HttpEntity httpEntity = new HttpEntity(headers);
-//        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.exchange(ufsProviderUrl + "/" + endpoint, HttpMethod.DELETE, httpEntity, Void.class);
-//    }
-//
-//
-//}
+package com.platformv.dictionaries.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class UfsService {
+
+    @Value("${ufs.provider.url}")
+    private String ufsProviderUrl;
+
+    public <TResponse> TResponse get(String endpoint, Class<TResponse> responseClass) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<TResponse> result = restTemplate.exchange(ufsProviderUrl + "/" + endpoint,
+                HttpMethod.GET, null, responseClass);
+        return result.getBody();
+    }
+
+    public <TResponse, TRequest> TResponse post(String endpoint, TRequest request, Class<TResponse> responseClass) {
+        HttpEntity httpEntity = new HttpEntity(request);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<TResponse> result = restTemplate.postForEntity(ufsProviderUrl + "/" + endpoint, httpEntity, responseClass);
+        return result.getBody();
+    }
+
+    public <TRequest> void post(String endpoint, TRequest request) {
+        HttpEntity httpEntity = new HttpEntity(request);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForEntity(ufsProviderUrl + "/" + endpoint, httpEntity, Void.class);
+    }
+
+    public <TRequest> void put(String endpoint, TRequest request) {
+        HttpEntity httpEntity = new HttpEntity(request);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.exchange(ufsProviderUrl + "/" + endpoint, HttpMethod.PUT, httpEntity, Void.class);
+    }
+
+    public void delete(String endpoint) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.exchange(ufsProviderUrl + "/" + endpoint, HttpMethod.DELETE, null, Void.class);
+    }
+
+    public <TResponse> TResponse get(String endpoint, HttpHeaders headers, Class<TResponse> responseClass) {
+        HttpEntity httpEntity = new HttpEntity(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<TResponse> result = restTemplate.exchange(ufsProviderUrl + "/" + endpoint,
+                HttpMethod.GET, httpEntity, responseClass);
+        return result.getBody();
+    }
+
+    public <TResponse, TRequest> TResponse post(String endpoint, TRequest request, HttpHeaders headers, Class<TResponse> responseClass) {
+        HttpEntity httpEntity = new HttpEntity(request, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<TResponse> result = restTemplate.postForEntity(ufsProviderUrl + "/" + endpoint, httpEntity, responseClass);
+        return result.getBody();
+    }
+
+    public <TRequest> void post(String endpoint, TRequest request, HttpHeaders headers) {
+        HttpEntity httpEntity = new HttpEntity(request, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForEntity(ufsProviderUrl + "/" + endpoint, httpEntity, Void.class);
+    }
+
+    public <TRequest> void put(String endpoint, TRequest request, HttpHeaders headers) {
+        HttpEntity httpEntity = new HttpEntity(request, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.exchange(ufsProviderUrl + "/" + endpoint, HttpMethod.PUT, httpEntity, Void.class);
+    }
+
+    public void delete(String endpoint, HttpHeaders headers) {
+        HttpEntity httpEntity = new HttpEntity(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.exchange(ufsProviderUrl + "/" + endpoint, HttpMethod.DELETE, httpEntity, Void.class);
+    }
+
+
+}
